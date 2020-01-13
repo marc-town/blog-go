18e51602e6c1d8694189d1fc7bfceb0c035b71d1
marc-town <asamin.itconsultant@gmail.com>
2020-01-13 16:51:04 +0900

    header/footer新規作成

---
+++ web-gui/vue/src/components/parts/footer/Footer.vue
@@ -0,0 +1,19 @@
+<template src="./footer.html"></template>
+<script>
+export default {
+  components: {},
+  data () {
+    return {
+    }
+  },
+  mixins: [],
+  computed: {
+  },
+  watch: {},
+  methods: {
+  }
+}
+</script>
+<style scoped lang="scss">
+  @import "./footer.scss";
+</style>
---
+++ web-gui/vue/src/components/parts/header/Header.vue
@@ -0,0 +1,19 @@
+<template src="./header.html"></template>
+<script>
+export default {
+  components: {},
+  data () {
+    return {
+    }
+  },
+  mixins: [],
+  computed: {
+  },
+  watch: {},
+  methods: {
+  }
+}
+</script>
+<style scoped lang="scss">
+  @import "./header.scss";
+</style>
